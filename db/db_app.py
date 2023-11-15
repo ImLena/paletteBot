47b34da55a67412428074d6b1a9429fb066f8330
lena <evkhodosova@gmail.com>
2023-11-15 22:58:54 +0300

    add init table & requirements.txt

--- db/db_app.py
+++ db/db_app.py
@@ -22,3 +22,20 @@ def get_connection():
 
 def release_connection(conn):
     db_pool.putconn(conn)
+
+
+def setup_table():
+    connection = get_connection()
+    try:
+        with connection.cursor() as cursor:
+            cursor.execute("""CREATE TABLE IF NOT EXISTS usersInfo (
+                                id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+                                chat_id VARCHAR  UNIQUE,
+                                clusters INTEGER);
+                        """)
+            connection.commit()
+    except Exception as e:
+        print("Произошла ошибка: ", e)
+    finally:
+        release_connection(connection)
+    connection.commit()
